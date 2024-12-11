877a1b66fa77d04dbba618e8446896ea4b701c8b
Angel-Guiberteau <aguiberteaufranco.guadalupe@alumnado.fundacionloyola.net>
2024-12-11 12:26:06 +0100

    Modales funcionales

--- assets/js/unirseSala.js
+++ assets/js/unirseSala.js
@@ -1,3 +1,4 @@
+document.addEventListener("DOMContentLoaded", () => {
 // Referencias al modal y botones
 const modalUnirseSala = document.getElementById('modalUnirseSala');
 const unirseSalaButton = document.getElementById('unirseSalaButton');
@@ -14,11 +15,4 @@ closeModalUnirseSala.addEventListener('click', () => {
     modalUnirseSala.style.display = 'none';
 });
 
-// Manejar envío del formulario
-unirseSalaForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const codigoSala = document.getElementById('codigoSala').value;
-
-    // Redirigir a la misma vista con el código de la sala
-    window.location.href = `index.php?c=Usuarios&m=unirseSala&codigo=${codigoSala}`;
 });
